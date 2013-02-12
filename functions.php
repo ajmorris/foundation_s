e7560d159a7e8e6bd5358a032391271c7f2b07b1
AJ Morris <ajmorris2002@gmail.com>
2013-02-12 00:58:37 -0500

    Loading style.css

--- functions.php
+++ functions.php
@@ -95,9 +95,11 @@ add_action( 'widgets_init', 'foundation_s_widgets_init' );
  * Enqueue scripts and styles
  */
 function foundation_s_scripts() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
 	wp_register_style( 'foundation', get_template_directory_uri() . '/stylesheets/app.css' );
 	wp_enqueue_style( 'foundation' );
-	// wp_enqueue_style( 'style', get_stylesheet_uri() );
+	
 
 	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
 
