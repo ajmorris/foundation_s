3cabb93ddf5de4688aeb880406210c1f14907555
AJ Morris <ajmorris2002@gmail.com>
2013-02-12 01:07:32 -0500

    Basic CSS classes for Grid structure added
    Still need to add sub grid structure, nav, and a few others.

--- footer.php
+++ footer.php
@@ -9,17 +9,18 @@
  */
 ?>
 
-	</div><!-- #main .site-main -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'foundation_s_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'foundation_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'foundation_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'foundation_s' ), 'foundation_s', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon .site-footer -->
-</div><!-- #page .hfeed .site -->
+	</div><!-- #main .site-main .row -->
+    <div class="row">
+    	<footer id="colophon" class="site-footer" role="contentinfo">
+    		<div class="site-info">
+    			<?php do_action( 'foundation_s_credits' ); ?>
+    			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'foundation_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'foundation_s' ), 'WordPress' ); ?></a>
+    			<span class="sep"> | </span>
+    			<?php printf( __( 'Theme: %1$s by %2$s.', 'foundation_s' ), 'foundation_s', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+    		</div><!-- .site-info -->
+    	</footer><!-- #colophon .site-footer -->
+    </div>
+</div><!-- #page .hfeed .site .container -->
 
 <?php wp_footer(); ?>
 
